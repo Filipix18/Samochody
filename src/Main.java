c51c1ef8d4d6a43b7337cb3a60c11554ef4c160f
Filipix18 <120133100+Filipix18@users.noreply.github.com>
2025-03-27 21:53:13 +0100

    Position calculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,10 +10,18 @@ public class Main {
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        lemans.dodanie_samochod(100,100,5,0,0,0,255);
-        lemans.dodanie_samochod(120,120,5,0,12,14,18);
-        for (int i = 0; i < 80; i++) {
-            lemans.obliczane_pozycji(lemans.zbior_samochod.elementAt(0));
+        lemans.dodanie_samochod(75,150,5,Math.PI*1.5,0,0,255);
+        lemans.dodanie_samochod(75,150,5,0,102,14,18);
+        for (int i = 0; i < 300; i++) {
+            lemans.zbior_samochod.elementAt(0).kat += lemans.zbior_samochod.elementAt(0).losuj_przyrost_kata();  //Dodajemy losowy kat
+            lemans.zbior_samochod.elementAt(0).ustaw_kat();  //Resetowanie kata po przekroczeniu linii startu
+            lemans.zbior_samochod.elementAt(0).obliczane_pozycji();  //Obliczenie pozycji na podstawie kata
+            lemans.repaint();  // Odświeżenie okna
+            try {
+                Thread.sleep(100); // Opóźnienie 1000 ms (1 sekunda)
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
