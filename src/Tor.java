a870dde0e4afa4c671b021589da66782a43811db
Filipix18 <120133100+Filipix18@users.noreply.github.com>
2025-03-27 21:54:56 +0100

    Position calculation

--- src/Tor.java
+++ src/Tor.java
@@ -11,8 +11,6 @@ public class Tor extends JPanel {
 
     public void dodanie_samochod(int x, int y, int radius, double kat,int R, int G, int B ){
         zbior_samochod.addElement(new Samochody(x, y, radius, kat, R, G, B));
-
-
     }
 
     @Override
@@ -31,23 +29,14 @@ public class Tor extends JPanel {
         g.fillOval(100, 100, 100, 100);
     }
 
-    public void rys_samochody(Graphics g){
+    public void rys_samochody(Graphics g) {
         for (int i = 0; i < zbior_samochod.size(); i++) {
             g.setColor(zbior_samochod.elementAt(i).Daj_kolor());
-            g.fillOval(zbior_samochod.elementAt(i).x, zbior_samochod.elementAt(i).y,zbior_samochod.elementAt(i).radius,zbior_samochod.elementAt(i).radius);
+            g.fillOval(zbior_samochod.elementAt(i).x, zbior_samochod.elementAt(i).y, zbior_samochod.elementAt(i).radius, zbior_samochod.elementAt(i).radius);
 
-            new Thread(()->{
+            new Thread(() -> {
 
             }).start();
         }
-    }
-
-    public void obliczane_pozycji(Samochody auto){
-        int pozycja_poczX = auto.x;
-        int pozycja_poczY = auto.y;
-        auto.x += auto.radius*Math.sin(auto.losuj_przyrost_kata());
-        auto.y += auto.radius*Math.cos(auto.losuj_przyrost_kata());
-        System.out.print("x :"+auto.x);
-        System.out.println("---y :"+auto.y);
     }
 }
